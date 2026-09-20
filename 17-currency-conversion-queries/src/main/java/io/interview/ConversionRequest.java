0c2607df7151fbe1c4e79c19e22af4898aeee759
Swapnil Katkar <skatkar225@gmail.com>
2026-09-19 17:14:20 -0700

    Currency conversion with queries

---
+++ 17-currency-conversion-queries/src/main/java/io/interview/ConversionRequest.java
@@ -0,0 +1,25 @@
+package io.interview;
+
+public class ConversionRequest {
+    private final String source;
+    private final String target;
+    private final double amount;
+
+    public ConversionRequest(String source, String target, double amount) {
+        this.source = source;
+        this.target = target;
+        this.amount = amount;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+}
---
+++ 17-currency-conversion-queries/src/main/java/io/interview/Edge.java
@@ -0,0 +1,19 @@
+package io.interview;
+
+public class Edge {
+    private final String currency;
+    private final double rate;
+
+    public Edge(String currency, double rate) {
+        this.currency = currency;
+        this.rate = rate;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+}
