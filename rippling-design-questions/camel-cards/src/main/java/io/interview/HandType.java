51f1299989c758a390e0339c7aa02708a05bb220
Swapnil Katkar <skatkar225@gmail.com>
2026-09-19 21:43:34 -0700

    Rippling LLD questions

---
+++ rippling-design-questions/camel-cards/src/main/java/io/interview/HandType.java
@@ -0,0 +1,19 @@
+package io.interview;
+
+public enum HandType {
+    HIGH_CARD(1),
+    ONE_PAIR(2),
+    THREE_OF_A_KIND(3),
+    TWO_PAIR(4),
+    FOUR_OF_A_KIND(5);
+
+    private final int strength;
+
+    HandType(int strength) {
+        this.strength = strength;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+}
