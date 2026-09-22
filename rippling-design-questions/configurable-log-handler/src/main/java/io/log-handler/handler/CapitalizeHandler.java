24d5b3aec3e48a7f1c7df42ea02751939343588f
Swapnil Katkar <skatkar225@gmail.com>
2026-09-21 18:51:53 -0700

    Updating project structure

--- rippling-design-questions/configurable-log-handler/src/main/java/io/log-handler/handler/CapitalizeHandler.java
+++ rippling-design-questions/configurable-log-handler/src/main/java/io/log-handler/handler/CapitalizeHandler.java
@@ -2,7 +2,7 @@ package io.interview.handler;
 
 import io.interview.HandlerResult;
 
-public class CapitalizeHandler extends LogHandler{
+public class CapitalizeHandler extends io.interview.handler.LogHandler {
     @Override
     public HandlerResult handle(String message) {
         return new HandlerResult(message.toUpperCase(), true);
