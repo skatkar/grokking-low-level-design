bf60bdd17d83dbee1c22147d75c40cedc4826a41
Swapnil Katkar <skatkar225@gmail.com>
2026-09-21 18:52:14 -0700

    Updating project structure

---
+++ rippling-design-questions/currency-conversion-query/src/main/java/io/currency/Main.java
@@ -0,0 +1,26 @@
+package io.currency;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<ExchangeRate> rates = Arrays.asList(
+                new ExchangeRate("USD", "EUR", 0.85),
+                new ExchangeRate("EUR", "GBP", 0.88),
+                new ExchangeRate("GBP", "INR", 105.0)
+        );
+
+        List<ConversionRequest> queries = Arrays.asList(
+                new ConversionRequest("USD", "EUR", 100),
+                new ConversionRequest("USD", "GBP", 100),
+                new ConversionRequest("USD", "INR", 100),
+                new ConversionRequest("USD", "JPY", 100)
+        );
+
+        CurrencyConverter converter = new CurrencyConverter();
+        List<Double> results = converter.convertCurrency(rates, queries);
+
+        System.out.println(results);
+    }
+}
